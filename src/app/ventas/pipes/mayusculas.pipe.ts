5cf37a030dcf6b2ce88854a55312d827dfb3aa29
aiexgt <lima14florian14@gmail.com>
2022-10-29 23:33:09 -0600

    Recibir argumentos pipe

--- src/app/ventas/pipes/mayusculas.pipe.ts
+++ src/app/ventas/pipes/mayusculas.pipe.ts
@@ -5,7 +5,12 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class MayusculasPipe implements PipeTransform {
 
-    transform(value: any, ...args: any[]) {
-        return "HOLA MUNDO";
+    transform(value: string, enMayusculas: boolean = true): string {
+        if(enMayusculas){
+            return value.toUpperCase();
+        }else{
+            return value.toLocaleLowerCase();
+        }
+        
     }
 }
